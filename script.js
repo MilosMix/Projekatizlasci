d67c3c236b58b82f5d319fe2d1ed6454457b7c9c
Mix <mixgrabovac@gmail.com>
2023-05-17 12:47:25 +0200

    fixed index and addlisting adn added js file

---
+++ script.js
@@ -0,0 +1,17 @@
+const events = [];
+
+const eventTemplate = `<div>
+<img src="irishpub.jpg" alt="irskipub">
+<div class="txt">
+    <a href="https://www.instagram.com/the_king_of_irish_pub/">
+        <h3>The king of Irish Pub</h3>
+    </a>
+    <p>Muzika uživo
+        <br>petak 12.05.2023<br>
+        Trg Republike Srbije 1</p>
+</div>
+</div>`;
+
+document.getElementsByClassName('glavni-kontejner')[0].innerHTML += eventTemplate;
+
+
